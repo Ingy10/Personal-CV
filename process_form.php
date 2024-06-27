a32b2700dda1cb88cdf5e9e8fd7d9d8681f72cb1
Kyle Ingham <Kyle@MacBook-Pro.local>
2024-06-27 11:32:34 -0600

    Added PHP file to handle contact form submissions/added jukebox website to projects/removed embedded scratch game and replaced with .png + link.

---
+++ process_form.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $first_name = $_POST['first-name'];
+    $last_name = $_POST['last-name'];
+    $email = $_POST['email'];
+    $company = $_POST['company'];
+    $message = $_POST['message'];
+
+    $to = 'kyleingy@gmail.com'; // Replace with your email address
+    $subject = 'New Contact Form Submission - '$first_name;
+    $body = "Name: $first_name $last_name\n";
+    $body .= "Email: $email\n";
+    $body .= "Company: $company\n\n";
+    $body .= "Message:\n$message";
+
+    if(mail($to, $subject, $body)) {
+        echo 'Message sent successfully.';
+    } else {
+        echo 'Error: Unable to send message.';
+    }
+}
+?>
